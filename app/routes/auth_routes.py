c297425c922239c5db2795e49c116bd61841746f
Allan Pasion <PasionAllanJr@gmail.com>
2026-08-09 16:25:17 +0800

    Update auth_routes.py

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, render_template, redirect, url_for, flash, request
 from flask_login import login_user, current_user, logout_user, login_required
 from app import db, bcrypt
-from app.models import User 
+from app.models import User
 
-auth_bp = Blueprint('auth', __namename__)
+auth_bp = Blueprint('auth', __name__)
 
 def is_safe_next_url(target):
     return bool(target) and target.startswith('/') and not target.startswith('//')
